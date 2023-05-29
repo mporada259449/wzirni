903dc38170db8e1c80c0bd0f295158e6efbdd0e5
Kozlowski-Bartosz <kozlowski.bartosz@protonmail.com>
2023-05-29 19:07:36 +0200

    Now reads addresses from a file, output added, pathing changed.

--- src/main.py
+++ src/main.py
@@ -7,7 +7,7 @@ def read_log():
     #odczytanie adresów z logu
     #jak coś to te logi są tak zrobione że każda linia jest osobnym jsonem
     #zwraca listę ze wszystkimi adresami
-    cowriepath = input("Input log file path/name [cowrie.json]: ") or "cowrie.json"
+    cowriepath = input("Input log file path/name [output/cowrie.json]: ") or "output/cowrie.json"
     
     with open(cowriepath, "r") as log:
         all_addreses = set()
@@ -23,16 +23,19 @@ if __name__=="__main__":
     load_dotenv()
     key = os.getenv("API_KEY")
     ip_analyser = core.IP(api_key = key)
-    addreses = read_log()
-    print(f"Found {len(addreses)} unique IP addresses")
+    #addreses = read_log()
+    addressespath = input("Input address file path/name [source/addresses.txt]: ") or "source/addresses.txt"
+    
+    addresses = [line.strip() for line in open(addressespath, 'r')]
+    print(f"Found {len(addresses)} unique IP addresses")
     
     result = []
     
-    filepath = input("Input result file path/name [results.json]: ") or "results.json"
+    filepath = input("Input result file path/name [output/results.json]: ") or "output/results.json"
     
     
     print("Starting analysis...")
-    for ip in addreses:
+    for ip in addresses:
         print(f"Analysing IP: {ip}")
         analysis_result = ip_analyser.info_ip(ip)
         result.append(analysis_result)
@@ -40,5 +43,5 @@ if __name__=="__main__":
     
     
     with open(filepath, "w", encoding="utf-8") as f:
-        json.dump(result, f, indent=1)
+        json.dump(result, f, indent=2)
         print(f"Results saved to {filepath}")
